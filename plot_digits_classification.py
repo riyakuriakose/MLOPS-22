be4b898cbe7f2a9a876291b230c95506c5ef194f
Riya Kuriakose <kuriakose.1@iitj.ac.in>
2022-10-30 22:20:29 +0530

    moved hyperparameter tuning to utils

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,62 +1,67 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
-import matplotlib.pyplot as plt
-
-# Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split
-from utils import preprocess_digits,data_viz,train_dev_test_split,h_param_tuning,pred_image_viz
+import pdb
 
-gamma_list = [0.001,0.05,0.00001,0.0009,0.08]
-c_list = [0.2,0.5,0.01,0.9,5,3,2,0.1,0.004]
+from utils import (
+    preprocess_digits,
+    train_dev_test_split,
+    h_param_tuning,
+    data_viz,
+    pred_image_viz,
+    get_all_h_param_comb,
+    tune_and_save,
+)
+from joblib import dump, load
 
-h_param_comb = [{'gamma':g,'C':c} for g in gamma_list for c in c_list]
-assert len(h_param_comb) == len(gamma_list)*len(c_list)
+train_frac, dev_frac, test_frac = 0.8, 0.1, 0.1
+assert train_frac + dev_frac + test_frac == 1.0
 
+# 1. set the ranges of hyper parameters
+gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
+
+params = {}
+params["gamma"] = gamma_list
+params["C"] = c_list
+
+h_param_comb = get_all_h_param_comb(params)
+
+
+# PART: load dataset -- data from csv, tsv, jsonl, pickle
 digits = datasets.load_digits()
 data_viz(digits)
-
-data,label = preprocess_digits(digits)
-
-train_frac,dev_frac,test_frac = 0.8,0.1,0.1
-assert train_frac+dev_frac+test_frac==1.0
-
-x_train,y_train,x_dev,y_dev,x_test,y_test = train_dev_test_split(data,label,train_frac,dev_frac)
+data, label = preprocess_digits(digits)
+# housekeeping
+del digits
 
 
+x_train, y_train, x_dev, y_dev, x_test, y_test = train_dev_test_split(
+    data, label, train_frac, dev_frac
+)
 
+# PART: Define the model
+# Create a classifier: a support vector classifier
 clf = svm.SVC()
+# define the evaluation metric
 metric = metrics.accuracy_score
 
-best_model,best_acc,best_h_params  = h_param_tuning(h_param_comb,clf,x_train,y_train,x_dev,y_dev,metric)
+
+actual_model_path = tune_and_save(
+    clf, x_train, y_train, x_dev, y_dev, metric, h_param_comb, model_path=None
+)
 
 
+# 2. load the best_model
+best_model = load(actual_model_path)
 
+# PART: Get test set predictions
+# Predict the value of the digit on the test subset
 predicted = best_model.predict(x_test)
-pred_image_viz(predicted,x_test)
-    
 
+pred_image_viz(x_test, predicted)
 
+# 4. report the test set accurancy with that best model.
+# PART: Compute evaluation metrics
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
-
-# disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-# disp.figure_.suptitle("Confusion Matrix")
-# print(f"Confusion matrix:\n{disp.confusion_matrix}")
-plt.show()
-
-print("Best hyperparameters were: ")
-print(best_h_params)
-
-
-
