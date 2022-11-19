e610aa177042100e7206c8285d155bb78abc85c3
riyakuriakose <kuriakose.1@iitj.ac.in>
2022-11-19 11:48:58 +0530

    flask

--- api/app.py
+++ api/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app= Flask(__name__)
 
 @app.route("/hello_world")
 def hello_world():
     return("HI")
+    
+@app.route("/sum",methods=['POST'])
+def sum():
+    print(request.json)
+    x= request.json['x']
+    y= request.json['y']
+    z=x+y
+  
+    return jsonify({"sum":z})
     
     
 
